4319ef3745c834411eb7b8d6927319902f4ae29f
azhg.sabyrzhan@gmail.com <azhg.sabyrzhan@gmali.com>
2021-06-24 22:42:36 +0500

    connect database

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import express from "express"
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 dotenv.config();
 
 const app = express()
 const port = parseInt(process.env.PORT,10) || 5000
+const dbUrl = process.env.DB_URL;
 
 
 async function start() {
     try {
+        await mongoose.connect(dbUrl, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            connectTimeoutMS: 1000
+          });
         app.listen(port, () => {
             console.log(`server started at port: ${port}`)
         });
